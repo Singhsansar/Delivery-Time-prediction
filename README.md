caaea3d88cdad77cf31c4371f7c45de9c2e3a65d
Singhsansar <singhsansar433@gmail.com>
2024-02-24 11:30:11 +0530

    added the setup file and requirements

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-End-to-End Machine Learning project 
+# **End-to-End Machine Learning project**
+
+1. Clone the repo
+
+   ```bash
+   https://github.com/Singhsansar/Machine-Learning-Project.git
+   ```
+
+2. Create Virtual Environment
+   ```bash
+   python3 -m venv ~/.venv
+   ```
+3. Activate Virtual Environment
+
+   ```bash
+   source ~/.venv/bin/activate
+   ```
+
+4. Install the requirements
+   ```bash
+   pip install -r requirements.txt
+   ```
---
+++ setup.py
@@ -0,0 +1,50 @@
+import setuptools
+from setuptools import setup, find_packages
+from typing import List
+
+with open("README.md", 'r', encoding='utf-8') as f:
+    long_description = f.read()
+    
+__version__ = "0.0.1"
+NAME = "MachineLearningProject"
+REPONAME = 'Machine-Learning-Project'
+AUTHOR = "Nikhil Kumar Singh(Nikhil)"
+AUTHOR_EMAIL = "aiengineer@nikhilsingh.com.np"
+AUTHOR_USER_NAME = "singhsansar"
+REQUIREMENTS_FILE_NAME = "requirements.txt"
+
+
+HYPHEN_E_DOT = "-e ."
+# Requriments.txt file open
+# read
+# \n ""
+
+def get_requirements_list()->List[str]:
+    with open(REQUIREMENTS_FILE_NAME) as requriment_file:
+        requriment_list = requriment_file.readlines()
+        requriment_list = [requriment_name.replace("\n", "") for requriment_name in requriment_list]
+
+        if HYPHEN_E_DOT in requriment_list:
+            requriment_list.remove(HYPHEN_E_DOT)
+
+        return requriment_list
+
+
+
+setuptools.setup(
+    name=REPONAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small Machine Learning Project",
+    long_description=long_description, 
+    long_description_content_type="text/markdown",  
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPONAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPONAME}/issues",  
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),  # comma added here
+    install_requries = get_requirements_list()
+)
+

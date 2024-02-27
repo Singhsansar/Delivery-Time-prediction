4aec06a02416425f4d85de386a749cd328e3d531
Singhsansar <singhsansar433@gmail.com>
2024-02-27 09:10:09 +0530

    added dataset , manage exception handling and also the logs

---
+++ exceptions.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import os, sys
+from Machine_learning.logger import logging
+from Machine_learning.exception import CustomException
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    try: 
+        raise Exception("This is a test Exception file")
+    except Exception as e:
+        temp = CustomException(e)
+        logging.error(temp.error_message)  # Use logging.error to log exceptions
+        logging.info("we are testing our logging module.")  # Use logging.info for general logs
+        
+    return "testing the logging file success!"
+
+
+if __name__ == "__main__":
+    app.run(debug=False)  # Specify port 6000
--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 import setuptools
-from setuptools import setup, find_packages
 from typing import List
 
-with open("README.md", 'r', encoding='utf-8') as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
-    
+
 __version__ = "0.0.1"
 NAME = "MachineLearningProject"
-REPONAME = 'Machine-Learning-Project'
+REPONAME = "Machine-Learning-Project"
 AUTHOR = "Nikhil Kumar Singh(Nikhil)"
 AUTHOR_EMAIL = "aiengineer@nikhilsingh.com.np"
 AUTHOR_USER_NAME = "singhsansar"
@@ -19,16 +18,18 @@ HYPHEN_E_DOT = "-e ."
 # read
 # \n ""
 
-def get_requirements_list()->List[str]:
-    with open(REQUIREMENTS_FILE_NAME) as requriment_file:
+
+def get_requirements_list() -> List[str]:
+    with open(REQUIREMENTS_FILE_NAME, encoding="utf-8") as requriment_file:
         requriment_list = requriment_file.readlines()
-        requriment_list = [requriment_name.replace("\n", "") for requriment_name in requriment_list]
+        requriment_list = [
+            requriment_name.replace("\n", "") for requriment_name in requriment_list
+        ]
 
         if HYPHEN_E_DOT in requriment_list:
             requriment_list.remove(HYPHEN_E_DOT)
 
         return requriment_list
-
 
 
 setuptools.setup(
@@ -37,14 +38,13 @@ setuptools.setup(
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
     description="A small Machine Learning Project",
-    long_description=long_description, 
-    long_description_content_type="text/markdown",  
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPONAME}",
     project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPONAME}/issues",  
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPONAME}/issues",
     },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),  # comma added here
-    install_requries = get_requirements_list()
+    install_requries=get_requirements_list(),
 )
-
